54b1867aa697d91bd3fdca10ba2ce04d8287f7e4
sayedshaabn <90373841+sayedshaabn@users.noreply.github.com>
2021-09-15 21:55:58 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -3,7 +3,7 @@
 # SCRIPT : DOWNLOAD AND INSTALL JEDIEPGEXTRAM
 # ###########################################
 #
-# Command: wget https://raw.githubusercontent.com/emilnabil/jediepgextream/main/installer.sh -qO - | /bin/sh ###########################################
+# Command: wget https://raw.githubusercontent.com/sayedshaabn/jediepgextream/main/installer.sh -qO - | /bin/sh ###########################################
 # ###########################################
 echo " **********************************************************************************************************"
 # Configure where we can find things here #
